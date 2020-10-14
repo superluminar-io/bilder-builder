958d98703d818ae0a5b6c3b7a3e4b90f7fa878dd
Alexander Schramm <alex@schram.me>
2020-10-14 13:12:34 +0200

    chore: try to make cdk pipeline work

--- aws/stacks/Pipeline.ts
+++ aws/stacks/Pipeline.ts
@@ -1,19 +1,27 @@
-import * as codepipeline from '@aws-cdk/aws-codepipeline'
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core'
-import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines'
-
-import { BilderBuilderDeploymentStage } from '../stages/deploy'
+import * as Codepipeline from '@aws-cdk/aws-codepipeline'
+import * as CodepipelineActions from '@aws-cdk/aws-codepipeline-actions'
+import * as CodeBuild from '@aws-cdk/aws-codebuild'
+import * as S3 from '@aws-cdk/aws-s3'
+import { CfnOutput, Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core'
+import { CdkPipeline } from '@aws-cdk/pipelines'
 
 /**
  * The stack that defines the application pipeline
  */
 export class BilderBuilderPipelineStack extends Stack {
+  public readonly bucketDomain: CfnOutput
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const sourceArtifact = new codepipeline.Artifact()
-    const cloudAssemblyArtifact = new codepipeline.Artifact()
+    const websiteBucket = new S3.Bucket(this, 'Files', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true
+    })
+
+    const sourceArtifact = new Codepipeline.Artifact()
+    const buildArtifact = new Codepipeline.Artifact()
+    const cloudAssemblyArtifact = new Codepipeline.Artifact()
 
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       // The pipeline name
@@ -21,24 +29,42 @@ export class BilderBuilderPipelineStack extends Stack {
       cloudAssemblyArtifact,
 
       // Where the source can be found
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
+      sourceAction: new CodepipelineActions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
         oauthToken: SecretValue.secretsManager('github-token'),
         owner: 'superluminar-io',
         repo: 'bilder-builder'
-      }),
-
-      // How it will be built and synthesized
-      synthAction: SimpleSynthAction.standardYarnSynth({
-        sourceArtifact,
-        cloudAssemblyArtifact,
-
-        // We need a build step to compile the TypeScript Lambda
-        buildCommand: 'yarn build'
       })
     })
 
-    pipeline.addApplicationStage(new BilderBuilderDeploymentStage(this, 'Prod'))
+
+    const buildStage = pipeline.addStage('BuildBilderBuilderStage')
+
+    buildStage.addActions(
+      new CodepipelineActions.CodeBuildAction({
+        input: sourceArtifact,
+        outputs: [buildArtifact],
+        actionName: 'Build',
+        project: new CodeBuild.PipelineProject(this, 'BuildBilderBuilderProject', {
+          projectName: 'BuildBilderBuilderProject',
+        })
+      })
+    )
+
+    const deployStage = pipeline.addStage('DeployBilderBuilderStage')
+
+    deployStage.addActions(
+      new CodepipelineActions.S3DeployAction({
+        actionName: 'Deploy',
+        input: buildArtifact,
+        bucket: websiteBucket
+      })
+
+    )
+
+    this.bucketDomain = new CfnOutput(this, 'BucketDomain', {
+      value: websiteBucket.bucketWebsiteDomainName
+    })
   }
 }
