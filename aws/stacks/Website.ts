1890f8ca6d7d236cf693384f341905ad1e998bcb
Alexander Schramm <alex@schram.me>
2020-10-30 12:41:12 +0100

    feat: route53 alias record

--- aws/stacks/Website.ts
+++ aws/stacks/Website.ts
@@ -3,7 +3,8 @@ import * as S3 from '@aws-cdk/aws-s3'
 import { Distribution } from '@aws-cdk/aws-cloudfront'
 import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
 import { DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager'
-import { HostedZone } from '@aws-cdk/aws-route53'
+import { HostedZone, ARecord, RecordTarget } from '@aws-cdk/aws-route53'
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets'
 
 export interface WebsiteStackProps extends StackProps {
   domainName: string
@@ -29,10 +30,15 @@ export class WebsiteStack extends Stack {
       hostedZone: zone
     })
 
-    new Distribution(this, 'WebsiteDistribution', {
+    const distribution = new Distribution(this, 'WebsiteDistribution', {
       defaultBehavior: { origin: new S3Origin(bucket) },
       domainNames: [props.domainName],
       certificate: certificate
+    })
+
+    new ARecord(this, 'AliasRecord', {
+      zone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution))
     })
 
     new CfnOutput(this, 'BucketDomain', {
